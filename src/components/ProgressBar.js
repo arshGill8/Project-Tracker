b9f40bfe8f7c10b26b15950f82365980def1135d
arshgill8 <arsh95g@gmail.com>
2023-10-01 22:09:57 -0400

    progress bar done-cleaner

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,31 +1,17 @@
 import { useEffect, useState } from "react";
 
-export default function ProgressBar({ jobs, jobChecked }) {
+export default function ProgressBar({ jobs }) {
   const [progress, setProgress] = useState(0);
 
-  // Calculate the maximum value of the progress bar based on the number of jobs
-  // const maxProgressBarValue = (jobs.length / 5) * 100;
-  const maxProgressBarValue = jobs.length > 0 ? jobs.length : 1;
-
   useEffect(() => {
-    // Calculate the count of checked jobs
-    const checkedCount = jobs.reduce((acc, job) => {
-      return job.isChecked ? acc + 1 : acc;
-    }, 0);
+    const checkedJobs = jobs.filter((job) => job.isChecked);
 
-    const checkedJobsPercentage = (checkedCount / maxProgressBarValue) * 100;
+    const checkedJobsPercentage = (checkedJobs.length / jobs.length) * 100 || 0;
+
     setProgress(checkedJobsPercentage);
   }, [jobs]);
 
   const progressPercentage = Math.floor(progress);
-
-  //   const countWidth = (checkedCount / 5) * 100;
-  //   setProgress(countWidth);
-  // }, [jobs]);
-
-  // let progressPercentage = maxProgressBarValue
-  //   ? Math.floor((progress / maxProgressBarValue) * 100)
-  //   : 0;
 
   return (
     <div>
